517dc77e4956dc65472036e8c673645838d0100e
Danny Roubin <dannyroubin1@gmail.com>
2021-08-11 20:19:52 -0700

    there is now a working footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Container, Row, FooterLink, Heading } from "./FooterStyles";
+
+const Footer = () => {
+  return (
+    <Box>
+      <h3 style={{ color: "white", textAlign: "center", marginTop: "-50px" }}>
+        Made by Daniel Roubin
+      </h3>
+    </Box>
+  );
+};
+export default Footer;
